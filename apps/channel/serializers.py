96b1d3aa2a59618ca2c5af42020220f87035fcce
Tolik <tolikberdyev@gmail.com>
2023-01-14 19:04:15 +0300

    Сделал добавление пользователей и список добавленных пользователей

--- apps/channel/serializers.py
+++ apps/channel/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
 from apps.channel import models
+from apps.account.models import User
 from utils.validators import validate_letters
 from utils.img_to_webp import img_to_webp
 
@@ -34,4 +35,33 @@ class DetailChannelSerializers(ListChannelSerializers):
         return img_to_webp(instance)
 
 
+class ListUserSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'avatar', 'first_name', 'last_name', 'username')
 
+
+class AddUserInChannelSerializers(serializers.Serializer):
+    users = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
+
+    def validate(self, attrs):
+        if attrs.get('users') not in self.context.get('users_instance'):
+            raise serializers.ValidationError(
+                {'user': 'Пользователя которого вы выбрали скорее всего уже вступлен в канал'})
+        return attrs
+
+    def create(self, validated_data):
+        my_list = []
+        for user in validated_data.get('users'):
+            my_list.append(models.UsersChannelModel(channel=validated_data.get('channel'), user=user))
+        instance = models.UsersChannelModel.objects.bulk_create(my_list)
+
+        return instance
+
+
+class UserInChannelSerializers(serializers.ModelSerializer):
+    user = ListUserSerializers()
+
+    class Meta:
+        model = models.UsersChannelModel
+        fields = ('id', 'user', 'role')
